7968c26e8210f75004e2194cc428659141c7abe7
unknown <joks026@gmail.com>
2021-08-21 22:49:49 +0800

    seeding

--- app/Models/order.php
+++ app/Models/order.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class order extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)->withPivot('quantity');
+    }
 }
