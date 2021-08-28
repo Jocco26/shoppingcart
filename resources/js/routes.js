ba44e1af525353aa62864940a859b3ccd10d6b18
unknown <joks026@gmail.com>
2021-08-28 17:35:20 +0800

    building frontend

---
+++ resources/js/routes.js
@@ -0,0 +1,7 @@
+module.export = [
+    {
+        path:'/',
+        name:'products.index',
+        component:() => import('./routes/Products/Index.vue')
+    },
+]
---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel Vue Stripe Cashier</title>
+
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body class="antialliased">
+    <div class="container mx-auto">
+        <div id="app">
+            
+        </div>
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>  
+</body>
+</html>
