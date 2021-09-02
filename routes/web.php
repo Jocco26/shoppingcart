543662c1aad637c0cc080d3f393f0989bf0904df
unknown <joks026@gmail.com>
2021-09-02 22:31:26 +0800

    vue scaffolding added

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::any('/{any}', function() {
     return view('app');
 })->where('any','.*');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
