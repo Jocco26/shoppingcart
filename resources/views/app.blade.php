eb3c4b693181eb81ea415fb1a995704cba373a17
unknown <joks026@gmail.com>
2021-10-03 23:18:15 +0800

    updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>laravel Vue Stripe Cashier</title>
+    <meta name="csrf-token" content="{{ csrf_token()}}">
+    <script>window.laravel = { csrfToken: '{{ csrf_token() }}'}</script>
+    <title>Shopping cart</title>
 
     <link rel="stylesheet" href="">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
